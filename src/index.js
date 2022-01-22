93f9d38962428f01c2878826523573e365a1cf0c
Vilmantas <vilmantas.sudaris@gmail.com>
2022-01-22 22:27:53 +0200

    [UPD] color change and generation. Styling updates

--- src/index.js
+++ src/index.js
@@ -4,22 +4,34 @@ import './index.css';
 
 function QuoteText(props) {
     const styles = {
-        color: props.color
+        color: props.color,
+        display: "flex",
+        justifyContent: "center",
+        margin: "1rem 0",
+        padding: "1.5rem",
+        fontSize: "2rem"
+    }
+    const style2 = {
+        marginRight: "1rem"
     }
     return (
         <div id='text' style={styles}>
-            <i className="fas fa-quote-left"></i> {props.quote}
+            <i className="fas fa-quote-left" style={style2}></i>{props.quote}
+            {/* <div>{props.quote}</div> */}
         </div>
     );
 }
 
 function QuoteAuthor(props) {
     const styles = {
-        color: props.color
+        color: props.color,
+        display: "flex",
+        justifyContent: "flex-end",
+        margin: "1rem 0"
     }
     return (
         <div id='author' style={styles}>
-            -{props.author}
+            - {props.author}
         </div>
     );
 }
@@ -28,17 +40,18 @@ function QuoteAuthor(props) {
 function ShareButton(props) {
     const styles = {
         fontSize: "2rem",
-        color: props.color
+        color: props.color,
+        marginRight: "0.5rem"
     }
     if (props.media == "twitter") {
         return (
-            <a id='tweet-quote' href="twitter.com/intent/tweet" style={styles}>
+            <a id='tweet-quote' href="twitter.com/intent/tweet" style={styles} target="_blank">
                 <i className='fab fa-twitter-square'></i>
             </a>
         );
     } else {
         return (
-            <a id='tumblr-quote' href='#' style={styles}>
+            <a id='tumblr-quote' href='#' style={styles} target="_blank">
                 <i className='fab fa-tumblr-square'></i>
             </a>
         );
@@ -47,7 +60,12 @@ function ShareButton(props) {
 
 function NewQuoteButton(props) {
     const styles = {
-        backgroundColor: props.color
+        backgroundColor: props.color,
+        color: "white",
+        padding: "0.5rem 1rem",
+        borderRadius: 3,
+        border: "none",
+        userSelect: "none"
     }
     return (
         <button onClick={props.generateQuote} id="new-quote" style={styles}>
@@ -83,7 +101,8 @@ class QuoteBox extends React.Component {
     }
 
     generateQuote() {
-        const rand = Math.floor(Math.random()*this.quoteList.length+1);
+        const rand = Math.floor(Math.random()*this.quoteList.length);
+        this.props.generateColor();
         this.setState({
             quote: this.quoteList[rand].quote,
             author: this.quoteList[rand].author
@@ -94,13 +113,18 @@ class QuoteBox extends React.Component {
         const styles = {
             backgroundColor: "white",
             width: 550,
-            padding: "1rem"
+            padding: "1rem",
+            borderRadius: 5
+        }
+        const buttonContainer = {
+            display: "flex",
+            justifyContent: "space-between"
         }
         return (
             <div id='quote-box' style={styles}>
                 <QuoteText quote={this.state.quote} color={this.props.color}/>
                 <QuoteAuthor author={this.state.author} color={this.props.color}/>
-                <div>
+                <div style={buttonContainer}>
                     <div>
                         <ShareButton media="twitter" color={this.props.color}/>
                         <ShareButton media="tumblr" color={this.props.color}/>
@@ -118,8 +142,33 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            color: "green"
+            color: ""
         }
+        this.colors = [
+            '#16a085',
+            '#27ae60',
+            '#2c3e50',
+            '#f39c12',
+            '#e74c3c',
+            '#9b59b6',
+            '#FB6964',
+            '#342224',
+            '#472E32',
+            '#BDBB99',
+            '#77B1A9',
+            '#73A857'];
+        this.generateColor = this.generateColor.bind(this);
+    }
+
+    componentDidMount() {
+        this.generateColor();
+    }
+
+    generateColor() {
+        const random = Math.floor(Math.random()*this.colors.length);
+        this.setState({
+            color: this.colors[random]
+        });
     }
 
     render() {
@@ -134,7 +183,8 @@ class App extends React.Component {
 
         return (
             <div style={styles}>
-                <QuoteBox color={this.state.color} />
+                <QuoteBox color={this.state.color}
+                    generateColor={this.generateColor} />
             </div>);
     }
 }
